83138d216a49ed71d69a4ca79249502dfd85a0a4
Pavel Pokatilo <ambulostrator@mail.ru>
2016-09-21 21:32:12 +0300

    Test2

--- lu.uni.teaching.il2.tutorial.group9/src/lu/uni/teaching/il2/tutorial/group9/TutorialClass.java
+++ lu.uni.teaching.il2.tutorial.group9/src/lu/uni/teaching/il2/tutorial/group9/TutorialClass.java
@@ -2,7 +2,7 @@ package lu.uni.teaching.il2.tutorial.group9;
 
 public class TutorialClass {
 	public static void main(String[] string){
-		System.out.println("Yolo");
+		System.out.println("Yolo2");
 		}
 	//hello
 }
